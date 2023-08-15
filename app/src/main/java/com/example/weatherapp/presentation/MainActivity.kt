bd67e89c53e384829c3e5cb55f9e1b09aa0c00fa
Amir Assubayev <amir.assubayev@excelsior-solutions.de>
2023-08-15 11:07:26 +0200

    make screen scrollable

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -68,6 +70,7 @@ fun MainScreen(
             modifier = modifier
                 .fillMaxSize()
                 .background(CarolinaBlue)
+                .verticalScroll(rememberScrollState())
         ) {
             state.currentWeather?.let {
                 CurrentWeatherSection(weatherAtTimeUi = it)
